5ea35d044b579196dbe5c3d4df29604235c169c8
Neocentury <toshifumi.inaba@outlook.jp>
2021-03-11 10:44:21 +0900

    ユーザー名の表示

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,46 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
  def index
   @prototypes = Prototype.all
+ end
+
+ def new
+  @prototypes = Prototype.new
+end
+
+def create
+  Prototype.create(prototype_params)
+end
+
+def destroy
+  prototype = Prototype.find(params[:id])
+  prototype.destroy
+end
+
+def edit
+end
+
+def update
+  prototype = Prototype.find(params[:id])
+  prototype.update(prototype_params)
+end
+
+def show
+end
+
+private
+def prototype_params
+  params.require(:Prototype).permit(:name, :image, :text)
+end
+
+def set_tweet
+  @prototype = Prototype.find(params[:id])
+end
+
+def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
+  end
  end
 end
